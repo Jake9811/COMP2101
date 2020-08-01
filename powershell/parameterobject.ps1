aa50e71a565df932aa7a7d4d51f2310d4171b4d1
Jake9811 <59779322+Jake9811@users.noreply.github.com>
2020-07-31 23:57:43 -0400

    Created parameterobject.ps1

---
+++ powershell/parameterobject.ps1
@@ -0,0 +1,7 @@
+﻿Param ([Parameter(Mandatory=$true,
+                  ValueFromPipeline=$true,
+                  ValueFromPipelineByPropertyName=$true)]$Object)
+$ObjectTypeName = $Object.gettype().name
+"Type of the Object = $ObjectTypeName"
+$ObjectTypeFullName = $Object.GetType().fullname
+"Full Name of Type = $ObjectTypeFullName "
